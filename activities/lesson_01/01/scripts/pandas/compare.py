d571745521ab0708f8dfbe96623cd8090cb82510
David Cueli <david.cueli_chavarria@siemens.com>
2026-04-22 11:31:11 +0200

    split comparison tasks into folders by bigdata tools and creation of file .ipynb to use cells

---
+++ activities/lesson_01/01/scripts/pandas/compare.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import time
+
+timeInit = time.time()
+df_movies = pd.read_csv('../dataset/ml-20m/movies.csv')
+timeEnd = time.time()
+
+print(f"Time to load the dataset with Pandas: {timeEnd - timeInit:.4f} seconds")
+print(f"Time to load the dataset with Pandas: {timeEnd - timeInit:.2f} seconds")
+
+print(df_movies.head())
---
+++ activities/lesson_01/01/scripts/polars/compare.py
@@ -0,0 +1,11 @@
+import polars as pl
+import time
+
+timeInit = time.time()
+df_movies = pl.read_csv('../dataset/ml-20m/movies.csv')
+timeEnd = time.time()
+
+print(f"Time to load the dataset with Polars: {timeEnd - timeInit:.4f} seconds")
+print(f"Time to load the dataset with Polars: {timeEnd - timeInit:.2f} seconds")
+
+print(df_movies.head())
